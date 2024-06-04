88f6964a4d47cfc054272baacbe02d9985774603
Erick Kurniawan <erick.kurniawan@gmail.com>
2024-06-04 13:23:20 +0700

    menambahkan interface Category

---
+++ RapidBootcamp.ConsoleApp/DAL/CategoriesDAL.cs
@@ -0,0 +1,42 @@
+﻿using RapidBootcamp.ConsoleApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidBootcamp.ConsoleApp.DAL
+{
+    public class CategoriesDAL : ICategory
+    {
+        public Category Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetByCategoryName(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
