6ff0ee313311191323f37e9c34fda5c30ca2d602
Erick Kurniawan <erick.kurniawan@gmail.com>
2024-06-05 14:12:20 +0700

    menambahkan asp core project

---
+++ RapidBootcamp.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RapidBootcamp.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello ASP.NET Core MVC!");
+        }
+    }
+}
